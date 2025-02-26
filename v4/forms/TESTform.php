f103164c1c0cf147bf2a1bf200a6d0eb92553e05
Sebastien IOVLEFF <sebastien.iovleff@gmail.com>
2025-02-26 20:02:45 +0100

    fix test form

--- v4/forms/TESTform.php
+++ v4/forms/TESTform.php
@@ -1,7 +1,7 @@
 <?php
 
 // Traitement du formulaire si une commande est soumise
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["command"] && $_POST["action"] == "TEST")) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "TEST") {
     $_SESSION["last_command"] = ["cmd" => $_POST["command"], "output" => executeCommand($_POST["command"])];
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
